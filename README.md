c3ec76375fb0172c9b5158fb249d003903158fa7
rijun <richard.jung001@stud.fh-dortmund.de>
2023-06-14 21:25:50 +0200

    Audio files can be integrated into the core

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# karim-flam-trombone
+# Karim Flam Trombone
+
+Due to copyright issues, the audio files are not a part of this repo.
+They are expected to be found in the *audio* directory in the `.pcm` format, which can be generated with FFMpeg as 
+following: `ffmpeg.exe -i <input>.wav -f s16le -acodec pcm_s16le <output>.pcm`.
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <cstdio>
 #include "pico/stdlib.h"
-#include "btstack_audio.h"
+// #include "btstack_audio.h" TODO Remove as unnecessary
+
+extern int16_t return_start[];
+extern int16_t *return_end;
+extern int16_t absorb_start[];
 
 int main() {
     setup_default_uart();
-    printf("Hello, world!\n");
+    printf("Hello, world!: %d\n", return_end - return_start);
 
     return 0;
 }
